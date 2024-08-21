ea5b04a31cdb47852110fc71c9a430512b36a67e
david-chua <dp_chua@hotmail.com>
2024-08-20 21:12:02 -0400

    module 6 complete

--- ModernReactWithReduxCourse/books/src/App.js
+++ ModernReactWithReduxCourse/books/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
-
+import BookList from './components/BookList';
 
 function App(){
     const [ books, setBooks] = useState([]);
@@ -9,14 +9,37 @@ function App(){
         console.log('Need to add book with: ', title);
         const updatedBooks = [
             ...books,
-            {id: 123, title: title}
+            { id: Math.round(Math.random() * 999), title }
         ]
 
         setBooks(updatedBooks);
     }
 
+    const editBookById = (id, newTitle) => {
+        console.log(newTitle);
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return {...book, title: newTitle}
+            }
+
+            return book; 
+        })
+
+        setBooks(updatedBooks);
+    }
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        })
+
+        setBooks(updatedBooks);
+    }
+
     return (
-        <div>
+        <div className="app">
+            <h1>Reading List</h1>
+            <BookList books={books} onDelete = {deleteBookById } onEdit={editBookById} />
             <BookCreate onCreate={createBook} />
         </div>
     )
