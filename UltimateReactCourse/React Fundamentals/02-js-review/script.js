c10b07591691ba1ff68a14056da4be199c21466f
David Chua <dp_chua@hotmail.com>
2025-08-27 01:08:38 -0400

    completed js essentials

--- UltimateReactCourse/React Fundamentals/02-js-review/script.js
+++ UltimateReactCourse/React Fundamentals/02-js-review/script.js
@@ -325,7 +325,11 @@ async function getTodos() {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const data = await res.json();
   console.log(data);
+
+  return data;
 }
 
 
-getTodos();
+const todos = getTodos();
+console.log(todos);
+console.log("Jonas");
