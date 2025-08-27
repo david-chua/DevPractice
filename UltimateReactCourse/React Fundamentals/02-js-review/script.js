edd52a2cf6b66f456f12c4f49fcf471b070193c5
David Chua <dp_chua@hotmail.com>
2025-08-27 01:04:41 -0400

    completed essential JS section

--- UltimateReactCourse/React Fundamentals/02-js-review/script.js
+++ UltimateReactCourse/React Fundamentals/02-js-review/script.js
@@ -315,3 +315,17 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
   book.id === 1 ? { ...book, pages: 1210 } : book
 );
 console.log(booksAfterUpdate);
+
+
+// Working with Promises
+
+// fetch("https://jsonplaceholder.typicode.com/todos").then((res) => res.json()).then((data) => console.log(data));
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+
+
+getTodos();
